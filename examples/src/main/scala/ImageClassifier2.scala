08ae3f0b980dfb6f3129d240b1dc141a43af416d
hmf <hmf@inescporto.pt>
2023-11-27 09:06:47 +0000

    PyT 2.1 setup does not detect GPU (Mill and SBT)

---
+++ examples/src/main/scala/ImageClassifier2.scala
@@ -0,0 +1,365 @@
+/*
+ * Copyright 2022 storch.dev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ // cSpell:ignore CUDA, MNIST
+ // cSpell:ignore sonatype, progressbar, redist
+ 
+package commands
+
+//> using scala "3.3"
+//> using repository "sonatype:snapshots"
+//> using repository "sonatype-s01:snapshots"
+//> using lib "dev.storch::vision:0.0-bbdc238-SNAPSHOT"
+//> using lib "me.tongfei:progressbar:0.9.5"
+//> using lib "com.github.alexarchambault::case-app:2.1.0-M24"
+//> using lib "org.scala-lang.modules::scala-parallel-collections:1.0.4"
+// replace with pytorch-platform-gpu if you have a CUDA capable GPU
+//> using lib "org.bytedeco:pytorch-platform:2.0.1-1.5.10-SNAPSHOT"
+// enable for CUDA support
+////> using lib "org.bytedeco:cuda-platform-redist:12.1-8.9-1.5.10-SNAPSHOT"
+
+import ImageClassifier.{Prediction, predict, train}
+import caseapp.*
+import caseapp.core.argparser.{ArgParser, SimpleArgParser}
+import caseapp.core.app.CommandsEntryPoint
+import com.sksamuel.scrimage.{ImmutableImage, ScaleMethod}
+import me.tongfei.progressbar.{ProgressBar, ProgressBarBuilder}
+import org.bytedeco.javacpp.PointerScope
+import org.bytedeco.pytorch.{InputArchive, OutputArchive}
+import os.Path
+import torch.*
+import torch.Device.{CPU, CUDA}
+import torch.optim.Adam
+import torchvision.models.resnet.{ResNet, ResNetVariant}
+
+import java.nio.file.Paths
+import scala.collection.parallel.CollectionConverters.ImmutableSeqIsParallelizable
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Await, Future}
+import scala.util.{Random, Try, Using}
+
+// format: off
+/** Example script for training an image-classification model on your own images.
+  *
+  * Usage: scala-cli ImageClassifier.scala -- train <dataset>
+  *  
+  * Where the expected dataset is a directory per class with examples, like this:
+  * .
+  * ├── PetImages
+  *     ├── Cat
+  *     │   ├── 1.jpg
+  *     │   ├── 2.jpg
+  *     │   ├── ...
+  *     └── Dog
+  *         ├── 1.jpg
+  *         ├── 2.jpg
+  *         ├── ...
+  * 
+  * To see all options run scala-cli ImageClassifier.scala -- -h
+  */
+//format: on
+object ImageClassifier extends CommandsEntryPoint:
+
+  val fileTypes = Seq("jpg", "png")
+
+  case class Metrics(loss: Float, accuracy: Float)
+
+  torch.manualSeed(0)
+  val random = new Random(seed = 0)
+
+  extension (number: Double) def format: String = "%1.5f".format(number)
+
+  def train(options: TrainOptions): Unit =
+    val device = if torch.cuda.isAvailable then CUDA else CPU
+    println(s"Using device: $device")
+
+    val datasetDir = os.Path(options.datasetDir, base = os.pwd)
+
+    /** verify that we can read all images of the dataset */
+    if os
+        .walk(datasetDir)
+        .filter(path => fileTypes.contains(path.ext))
+        .par
+        .map { path =>
+          val readTry = Try(ImmutableImage.loader().fromPath(path.toNIO)).map(_ => ())
+          readTry.failed.foreach(e => println(s"Could not read $path"))
+          readTry
+        }
+        .exists(_.isFailure)
+    then
+      println("Could not read all images in the dataset. Stopping.")
+      System.exit(1)
+    val classes = os.list(datasetDir).filter(os.isDir).map(_.last).sorted
+    val classIndices = classes.zipWithIndex.toMap
+    println(s"Found ${classIndices.size} classes: ${classIndices.mkString("[", ", ", "]")}")
+    val pathsPerLabel = classes.map { label =>
+      label -> os
+        .list(datasetDir / label)
+        .filter(path => fileTypes.contains(path.ext))
+    }.toMap
+    val pathsWithLabel =
+      pathsPerLabel.toSeq.flatMap((label, paths) => paths.map(path => path -> label))
+    println(s"Found ${pathsWithLabel.size} examples")
+    println(
+      pathsPerLabel
+        .map((label, paths) => s"Found ${paths.size} examples for class $label")
+        .mkString("\n")
+    )
+
+    val sample = random.shuffle(pathsWithLabel).take(options.take.getOrElse(pathsWithLabel.length))
+    val (trainData, testData) = sample.splitAt((sample.size * 0.9).toInt)
+    println(s"Train size: ${trainData.size}")
+    println(s"Eval size:  ${testData.size}")
+
+    val model: ResNet[Float32] = options.baseModel.factory(numClasses = classes.length)
+    println(s"Model architecture: ${options.baseModel}")
+    val transforms = options.baseModel.factory.DEFAULT.transforms
+
+    if options.pretrained then
+      println(s"Loading pre-trained model from: ${options.baseModel.factory.DEFAULT.url}")
+      val weights = torch.hub.loadStateDictFromUrl(options.baseModel.factory.DEFAULT.url)
+      // Don't load the classification head weights, as we they are specific to the imagenet classes
+      // and their output size (1000) usually won't match the number of classes of our dataset.
+      model.loadStateDict(
+        weights.filterNot((k, v) => Set("fc.weight", "fc.bias").contains(k))
+      )
+    model.to(device)
+
+    val optimizer = Adam(model.parameters, lr = options.learningRate)
+    val lossFn = torch.nn.loss.CrossEntropyLoss()
+    val numEpochs = options.epochs
+    val batchSize = options.batchSize
+    val trainSteps = (trainData.size / batchSize.toFloat).ceil.toInt
+    val evalSteps = (testData.size / options.batchSize.toFloat).ceil.toInt
+
+    // Lazily loads inputs and transforms them into batches of tensors in the shape the model expects.
+    def dataLoader(
+        dataset: Seq[(Path, String)],
+        shuffle: Boolean,
+        batchSize: Int
+    ): Iterator[(Tensor[Float32], Tensor[Int64])] =
+      val loader = ImmutableImage.loader()
+      (if shuffle then random.shuffle(dataset) else dataset)
+        .grouped(batchSize)
+        .map { batch =>
+          val (inputs, labels) = batch.unzip
+          // parallelize loading to improve GPU utilization
+          val transformedInputs =
+            inputs.par.map(path => transforms.transforms(loader.fromPath(path.toNIO))).seq
+          assert(transformedInputs.forall(t => !t.isnan.any.item))
+          (
+            transforms.batchTransforms(torch.stack(transformedInputs)),
+            torch.stack(labels.map(label => Tensor(classIndices(label)).to(dtype = int64)))
+          )
+        }
+
+    def trainDL = dataLoader(trainData, shuffle = true, batchSize)
+
+    def evaluate(): Metrics =
+      val testDL = dataLoader(testData, shuffle = false, batchSize = batchSize)
+      val evalPB =
+        ProgressBarBuilder().setTaskName(s"Evaluating        ").setInitialMax(evalSteps).build()
+      evalPB.setExtraMessage(s" " * 36)
+      val isTraining = model.isTraining
+      if isTraining then model.eval()
+      val (loss, correct) = testDL
+        .map { (inputBatch, labelBatch) =>
+          // make sure we deallocate intermediate tensors in time
+          Using.resource(new PointerScope()) { p =>
+            val pred = model(inputBatch.to(device))
+            val label = labelBatch.to(device)
+            val loss = lossFn(pred, label).item
+            val correct = pred.argmax(dim = 1).eq(label).sum.item
+            evalPB.step()
+            (loss, correct)
+          }
+        }
+        .toSeq
+        .unzip
+      val metrics = Metrics(
+        Tensor(loss).mean.item,
+        Tensor(correct).sum.item / testData.size.toFloat
+      )
+      evalPB.setExtraMessage(
+        s"    Loss: ${metrics.loss.format}, Accuracy: ${metrics.accuracy.format}"
+      )
+      evalPB.close()
+      if isTraining then model.train()
+      metrics
+
+    for epoch <- 1 to numEpochs do
+      val trainPB = ProgressBarBuilder()
+        .setTaskName(s"Training epoch $epoch/$numEpochs")
+        .setInitialMax(trainSteps)
+        .build()
+      var runningLoss = 0.0
+      var step = 0
+      var evalMetrics: Metrics = Metrics(Float.NaN, accuracy = 0)
+      for (input, label) <- trainDL do {
+        optimizer.zeroGrad()
+        // Using PointerScope ensures that all intermediate tensors are deallocated in time
+        Using.resource(new PointerScope()) { p =>
+          val pred = model(input.to(device))
+          val loss = lossFn(pred, label.to(device))
+          loss.backward()
+          // add a few sanity checks
+          assert(
+            model.parameters.forall(p => !p.isnan.any.item),
+            "Parameters containing nan values"
+          )
+          assert(
+            model.parameters.forall(p => !p.grad.exists(g => g.isnan.any.item)),
+            "Gradients containing nan values"
+          )
+          optimizer.step()
+          runningLoss += loss.item
+        }
+        trainPB.setExtraMessage(" " * 21 + s"Loss: ${(runningLoss / step).format}")
+        trainPB.step()
+        if ((step + 1) % (trainSteps / 4.0)).toInt == 0 then
+          evalMetrics = evaluate()
+          runningLoss = 0.0
+        step += 1
+      }
+      trainPB.close()
+      println(
+        s"Epoch $epoch/$numEpochs, Training loss: ${(runningLoss / step).format}, Evaluation loss: ${evalMetrics.loss.format}, Accuracy: ${evalMetrics.accuracy.format}"
+      )
+      val checkpointDir = os.Path(options.checkpointDir, os.pwd) / "%02d".format(epoch)
+      os.makeDir.all(checkpointDir)
+      val oa = OutputArchive()
+      model.to(CPU).save(oa)
+      oa.save_to((checkpointDir / "model.pt").toString)
+      os.write(checkpointDir / "model.txt", options.baseModel.toString())
+      os.write(checkpointDir / "classes.txt", classes.mkString("\n"))
+
+  case class Prediction(label: String, confidence: Double)
+
+  def predict(options: PredictOptions): Prediction =
+    val modelDir = options.modelDir match
+      case None =>
+        val checkpoints = os.list(os.pwd / "checkpoints", sort = true)
+        if checkpoints.isEmpty then
+          println("Not checkpoint found. Did you train a model?")
+          System.exit(1)
+        checkpoints.last
+      case Some(value) => os.Path(value, os.pwd)
+    println(s"Trying to load model from $modelDir")
+    val classes = os.read.lines(modelDir / "classes.txt")
+    val modelVariant =
+      ResNetVariant.valueOf(os.read(modelDir / "model.txt"))
+    val model: ResNet[Float32] = modelVariant.factory(numClasses = classes.length)
+    val transforms = modelVariant.factory.DEFAULT.transforms
+    val ia = InputArchive()
+    ia.load_from((modelDir / "model.pt").toString)
+    model.load(ia)
+    model.eval()
+    val image = ImmutableImage.loader().fromPath(Paths.get(options.imagePath))
+    val transformedImage =
+      transforms.batchTransforms(transforms.transforms(image)).unsqueeze(dim = 0)
+    val prediction = model(transformedImage)
+    val TensorTuple(confidence, index) =
+      torch.nn.functional.softmax(prediction, dim = 1)().max(dim = 1)
+    val predictedLabel = classes(index.item.toInt)
+    Prediction(predictedLabel, confidence.item)
+
+  override def commands: Seq[Command[?]] = Seq(Train, Predict)
+  override def progName: String = "image-classifier"
+
+implicit val customArgParser: ArgParser[ResNetVariant] =
+  SimpleArgParser.string.xmap(_.toString(), ResNetVariant.valueOf)
+
+@HelpMessage("Train an image classification model")
+case class TrainOptions(
+    @HelpMessage(
+      "Path to images. Images are expected to be stored in one directory per class i.e. cats/cat1.jpg cats/cat2.jpg dogs/dog1.jpg ..."
+    )
+    datasetDir: String,
+    @HelpMessage(
+      s"ResNet variant to use. Possible values are: ${ResNetVariant.values.mkString(", ")}. Defaults to ResNet50."
+    )
+    baseModel: ResNetVariant = ResNetVariant.ResNet50,
+    @HelpMessage("Load pre-trained weights for base-model")
+    pretrained: Boolean = true,
+    @HelpMessage("Where to save model checkpoints")
+    checkpointDir: String = "checkpoints",
+    @HelpMessage("The maximum number of images to take for training")
+    take: Option[Int] = None,
+    batchSize: Int = 8,
+    @HelpMessage("How many epochs (iterations over the input data) to train")
+    epochs: Int = 1,
+    learningRate: Double = 1e-5
+)
+@HelpMessage("Predict which class an image belongs to")
+case class PredictOptions(
+    @HelpMessage("Path to an image whose class we want to predict")
+    imagePath: String,
+    @HelpMessage(
+      "Path to to the serialized model created by running 'train'. Tries the latest model in 'checkpoints' if not set."
+    )
+    modelDir: Option[String] = None
+)
+
+object Commands:
+  object Train extends Command[TrainOptions]:
+    override def run(options: TrainOptions, remainingArgs: RemainingArgs): Unit = train(options)
+  object Predict extends Command[PredictOptions]:
+    override def run(options: PredictOptions, remainingArgs: RemainingArgs): Unit =
+      val Prediction(label, confidence) = predict(options)
+      println(s"Class: $label, confidence: $confidence")
+
+/**
+ * ./mill examples.runMain commands.Train -h
+ * ./mill examples.runMain commands.Train --dataset-dir /mnt/ssd2/hmf/datasets/computer_vision/kaggle_cats_and_dogs/pet_images --checkpoint-dir ~/.cache/storch/hub/checkpoints
+ * ./mill examples.runMain commands.Train --dataset-dir ../kagglecatsanddogs_3367a/PetImages --checkpoint-dir ~/.cache/storch/hub/checkpoints
+ * 
+ * 
+ * Caused by: java.lang.ExceptionInInitializerError: Exception java.lang.UnsatisfiedLinkError: /usr/local/sdkman/candidates/java/17.0.8-ms/lib/libawt_xawt.so: libXext.so.6: cannot open shared object file: No such file or directory [in thread "scala-execution-context-global-22"]
+ * -Djava.awt.headless=true
+ * 
+ * https://stackoverflow.com/questions/45531202/start-java-application-without-graphics-environment
+ * docker run -d \
+ *    -v /tmp/.X11-unix:/tmp/.X11-unix \
+ *    -v /etc/localtime:/etc/localtime \
+ *    -e DISPLAY=unix$DISPLAY \
+ *    <yourcontainer>
+ *   - https://stackoverflow.com/questions/40499412/how-to-view-gui-apps-from-inside-a-docker-container
+ *   - https://stackoverflow.com/questions/42181805/x11-forwarding-of-gui-app-in-docker-container
+ *   - 
+ * 
+ * https://stackoverflow.com/questions/67720077/java-openjdk-error-cannot-load-library-java-awt
+ * 
+ * @see https://www.kaggle.com/datasets/karakaggle/kaggle-cat-vs-dog-dataset
+ * @see https://www.microsoft.com/en-us/download/details.aspx?id=54765 (buggy)
+ */      
+object Train extends Command[TrainOptions]:
+  override def run(options: TrainOptions, remainingArgs: RemainingArgs): Unit = train(options)
+  override def main(args: Array[String]): Unit = 
+    if (args.isEmpty)
+    then
+      fullHelpAsked(finalHelp.progName)
+    else 
+      super.main(args)
+
+
+/**
+ * $ ./mill examples.runMain commands.Predict --dataset-dir ./data/mnist
+ */
+object Predict extends Command[PredictOptions]:
+  override def run(options: PredictOptions, remainingArgs: RemainingArgs): Unit =
+    val Prediction(label, confidence) = predict(options)
+    println(s"Class: $label, confidence: $confidence")
---
+++ examples/src/main/scala/LeNet2.scala
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2022 storch.dev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//> using scala "3.3"
+//> using repository "sonatype:snapshots"
+//> using repository "sonatype-s01:snapshots"
+//> using lib "dev.storch::vision:0.0-bbdc238-SNAPSHOT"
+// replace with pytorch-platform-gpu if you have a CUDA capable GPU
+//> using lib "org.bytedeco:pytorch-platform:2.0.1-1.5.10-SNAPSHOT"
+// enable for CUDA support
+////> using lib "org.bytedeco:cuda-platform-redist:12.1-8.9-1.5.10-SNAPSHOT"
+
+import torch.*
+import torch.nn.functional as F
+import torch.optim.Adam
+import org.bytedeco.pytorch.OutputArchive
+import torchvision.datasets.MNIST
+import scala.util.Random
+import java.nio.file.Paths
+import torch.Device.CUDA
+import scala.util.Using
+import org.bytedeco.javacpp.PointerScope
+import torch.Device.CPU
+import torch.nn.modules.HasParams
+
+// Define the model architecture
+class LeNet2[D <: BFloat16 | Float32: Default] extends HasParams[D] {
+
+  val conv1 = register(nn.Conv2d(1, 6, 5))
+    val conv2 = register(nn.Conv2d(6, 16, 5))
+  val fc1 = register(nn.Linear(16 * 4 * 4, 120))
+  val fc2 = register(nn.Linear(120, 84))
+  val fc3 = register(nn.Linear(84, 10))
+
+  def apply(i: Tensor[D]): Tensor[D] =
+    var x = F.maxPool2d(F.relu(conv1(i)), (2, 2))
+    x = F.maxPool2d(F.relu(conv2(x)), 2)
+    x = x.view(-1, 16 * 4 * 4) // all dimensions except the batch dimension
+    x = F.relu(fc1(x))
+    x = F.relu(fc2(x))
+    x = fc3(x)
+    x
+
+  //override def hasBias(): Boolean = modules.exists(_.hasBias())
+}
+
+/** 
+ * Shows how to train a simple LeNet on the MNIST dataset 
+ * 
+ * Run original code:
+ * ./mill examples.runMain LeNetApp
+ * Run this version
+ * ./mill examples.runMain LeNetApp2
+ * ./millw __.prepareOffline
+ * nohup ./millw __.prepareOffline > prep_offline.txt 2>&1 &
+ */
+object LeNetApp2 extends App {
+  val device = if torch.cuda.isAvailable then CUDA else CPU
+  println(s"Using device: $device")
+  val model = LeNet2().to(device)
+
+  // prepare data
+  val dataPath = Paths.get("data/mnist")
+  val mnistTrain = MNIST(dataPath, train = true, download = true)
+  val mnistEval = MNIST(dataPath, train = false)
+  val evalFeatures = mnistEval.features.to(device)
+  val evalTargets = mnistEval.targets.to(device)
+  val r = Random(seed = 0)
+
+  def dataLoader: Iterator[(Tensor[Float32], Tensor[Int64])] =
+    r.shuffle(mnistTrain).grouped(32).map { batch =>
+      val (features, targets) = batch.unzip
+      (torch.stack(features).to(device), torch.stack(targets).to(device))
+    }
+
+  val lossFn = torch.nn.loss.CrossEntropyLoss()
+  // enable AMSGrad to avoid convergence issues
+  val optimizer = Adam(model.parameters, lr = 1e-3, amsgrad = true)
+
+  // run training
+  for (epoch <- 1 to 5) do
+    for (batch <- dataLoader.zipWithIndex) do
+      // make sure we deallocate intermediate tensors in time
+      Using.resource(new PointerScope()) { p =>
+        val ((feature, target), batchIndex) = batch
+        optimizer.zeroGrad()
+        val prediction = model(feature)
+        val loss = lossFn(prediction, target)
+        loss.backward()
+        optimizer.step()
+        if batchIndex % 200 == 0 then
+          // run evaluation
+          val predictions = model(evalFeatures)
+          val evalLoss = lossFn(predictions, evalTargets)
+          val accuracy =
+            (predictions.argmax(dim = 1).eq(evalTargets).sum / mnistEval.length).item
+          println(
+            f"Epoch: $epoch | Batch: $batchIndex%4d | Training loss: ${loss.item}%.4f | Eval loss: ${evalLoss.item}%.4f | Eval accuracy: $accuracy%.4f"
+          )
+      }
+    val archive = new OutputArchive
+    model.save(archive)
+    archive.save_to("net.pt")
+}
